91783139a4ad7e1cb7e459c9ec1327ca9a0c998e
natalia soares <nsoaressousa@gmail.com>
2017-02-03 16:49:49 -0300

    adjust responsive menu close

--- assets/javascripts/action.js
+++ assets/javascripts/action.js
@@ -1,32 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-   $( document ).tooltip();
+  $(document).tooltip();
 
   // scroll links menu
-  $(".scroll-link").click(function() {
-      var ScrollOffset = $(this).attr('data-soffset');
-      //alert(ScrollOffset);
-      $("html, body").animate({
-          scrollTop: $($(this).attr("href")).offset().top - ScrollOffset + "px"
-      }, {
-          duration: 800,
-          easing: "easeInOutCubic"
-      });
-      return false;
-  });
-
-
-  $(".tooltip").click(function() {
-    $('.modalDialog').show();
-    $('#'+$(this).data('attr')).show();
+  $(".scroll-link").click(function () {
+    var ScrollOffset = $(this).attr('data-soffset');
+    //alert(ScrollOffset);
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top - ScrollOffset + "px"
+    }, {
+      duration: 800,
+      easing: "easeInOutCubic"
+    });
     return false;
   });
 
-  $(".close").click(function() {
-      $('.information').hide();
-      $('.modalDialog').hide();
+  // Close Menu on Responsive
+  $(document).on('click', function (e) {
+    if (e.target.id != 'main-menu' && e.target.id != 'sidebartoggle') {
+      $('#sidebartoggle').removeAttr('checked');
+    }
   });
 
 
+  $(".tooltip").click(function () {
+    $('.modalDialog').show();
+    $('#' + $(this).data('attr')).show();
+    return false;
+  });
+
+  $(".close").click(function () {
+    $('.information').hide();
+    $('.modalDialog').hide();
+  });
 
 });
